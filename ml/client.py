38894c99e0f4cf8679c8dde2ad117e1d86f9f241
mohamedElsherkawyy <mohamedmotaz942@gmail.com>
2025-05-02 16:22:18 +0300

    correct URL for exercise model endpoint

--- ml/client.py
+++ ml/client.py
@@ -1,6 +1,6 @@
 import httpx
 async def call_ml_model(data: dict):
-    url = "exercise-plan-production.up.railway.app"  # Exercise model endpoint
+    url = "https://exercise-plan-production.up.railway.app/predict"  # Exercise model endpoint
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=data)
         response.raise_for_status()
