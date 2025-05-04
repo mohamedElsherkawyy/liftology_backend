d0b4c183d4bb5454cfe319afedbe0fdb8ddfcb4c
mohamedElsherkawyy <mohamedmotaz942@gmail.com>
2025-05-04 17:04:25 +0300

    standardize field names in user registration and login

--- auth.py
+++ auth.py
@@ -4,29 +4,34 @@ from models import UserRegister, UserLogin
 
 router = APIRouter()
 
+
 @router.post("/signup")
 def signup(user: UserRegister):
     if users_col.find_one({"user_info.email": user.email}):
         raise HTTPException(status_code=400, detail="Email already exists")
-    users_col.insert_one({
-        "user_info":{
-        "first name": user.firstName,
-        "last name": user.lastName,
-        "email": user.email,
-        "password": user.password,
-        "phone number": user.phone,
-        "country": user.country,
+    users_col.insert_one(
+        {
+            "user_info": {
+                "firstName": user.firstName,
+                "last_name": user.lastName,
+                "email": user.email,
+                "password": user.password,
+                "phone": user.phone,
+                "country": user.country,
             },
-        "exercise_plan": None ,
-        "diet_plan": None,
-    })
-    return {"message": "User registered successfully",
-        "first_name": user["user_info"]["first name"],
-        "last_name": user["user_info"]["last name"],
+            "exercise_plan": None,
+            "diet_plan": None,
+        }
+    )
+    return {
+        "message": "User registered successfully",
+        "firstName": user["user_info"]["firstName"],
+        "lastName": user["user_info"]["lastName"],
         "email": user["user_info"]["email"],
-        "phone_number": user["user_info"]["phone number"],
-        "country": user["user_info"]["country"],    
-            }
+        "phone": user["user_info"]["phone"],
+        "country": user["user_info"]["country"],
+    }
+
 
 @router.post("/login")
 def login(data: UserLogin):
@@ -35,9 +40,9 @@ def login(data: UserLogin):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     return {
         "message": "Login successful",
-        "first_name": user["user_info"]["first name"],
-        "last_name": user["user_info"]["last name"],
+        "first_name": user["user_info"]["first_name"],
+        "last_name": user["user_info"]["last_name"],
         "email": user["user_info"]["email"],
-        "phone_number": user["user_info"]["phone number"],
-        "country": user["user_info"]["country"],    
+        "phone": user["user_info"]["phone"],
+        "country": user["user_info"]["country"],
     }
