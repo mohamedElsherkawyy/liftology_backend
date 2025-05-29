0c0188902479e3dbf8b8072456e43e187c9026fb
DESKTOP-GSECK01 <mostafa.elhwieg@gmail.com>
2025-05-29 08:28:23 +0300

    refactor: update update_full_plan endpoint to handle FullExercisePlan correctly

--- routes/plans.py
+++ routes/plans.py
@@ -3,7 +3,7 @@ from models import PlanRequest,FullExercisePlan
 from database import users_col
 from ml.client import call_ml_model
 from datetime import datetime
-
+import json
 router = APIRouter()
 
 @router.post("/generate/{user_email}")
@@ -32,10 +32,10 @@ def update_full_plan(email: str, new_plan: FullExercisePlan):
     user = users_col.find_one({"user_info.email": email})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-
+    new_plan=json.loads(new_plan)
     users_col.update_one(
         {"user_info.email": email},
-        {"$set": {"exercise_plan": new_plan.dict()}}
+        {"$set": {"exercise_plan": new_plan}}
     )
 
     return {"status": "success", "message": "Exercise plan updated successfully"}
